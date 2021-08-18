97fc2211de494a686ef82e122715386036efbe88
rupinderkaur <rupi063094@gmail.com>
2021-08-18 14:26:54 -0400

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById("create-task-form")
+form.addEventListener("submit", handleSubmit)
+const tasks = document.getElementById("tasks")
+
+function handleSubmit(e){
+  e.preventDefault()
+  const description = e.target["new-task-description"].value
+  addTask(description)
+  e.target.reset()
+}
+
+function addTask(description) {
+  const task = document.createElement("li")
+  task.innerText = description
+  tasks.appendChild(task)
+  const button = document.createElement("button")
+  button.innerText = "delete"
+  button.addEventListener("click", function(){
+    task.remove()
+  })
+  task.appendChild(button)
+  tasks.appendChild(task)
+}
+
+
+
+
